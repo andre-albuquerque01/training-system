d1c4863c2016e62ea9c3c8e04c4d86019c2d0a18
andre <andrealbuquerque2001@gmail.com>
2024-06-06 19:03:21 -0300

    feat: create job

---
+++ app/Jobs/SendVerifyEmail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Events\Registered;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendVerifyEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public string $email;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(string $email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        event(new Registered($this->email));
+    }
+}
