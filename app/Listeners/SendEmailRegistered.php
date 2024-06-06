d23fe604492bcfce729344f72c387bd4a8ab7053
andre <andrealbuquerque2001@gmail.com>
2024-06-06 19:03:31 -0300

    feat: create listeners

---
+++ app/Listeners/SendEmailRegistered.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\Registered;
+use App\Mail\VerifyEmail;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Mail;
+
+class SendEmailRegistered
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Registered $event): void
+    {
+        Mail::to($event->email)->send(new VerifyEmail([
+            'toEmail' => $event->email,
+            'subject' => 'Verificação de e-mail',
+            'message' => Crypt::encryptString($event->email)
+        ]));
+    }
+}
