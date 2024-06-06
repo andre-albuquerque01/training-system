e9983690f94b943f46648ea7c6e3a3c33e5e7e1b
andre <andrealbuquerque2001@gmail.com>
2024-06-06 18:51:56 -0300

    feat: update model

--- app/Models/Muscle.php
+++ app/Models/Muscle.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Muscle extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUlids;
 
-    protected $table = 'user_workOut';
+    protected $table = 'muscle';
 
     protected $primaryKey = 'idMuscle';
 
