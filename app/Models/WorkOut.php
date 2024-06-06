b2a2758e11b6b40fbebf1d174c05c6a75cd35827
andre <andrealbuquerque2001@gmail.com>
2024-06-06 18:51:45 -0300

    feat: update model

--- app/Models/WorkOut.php
+++ app/Models/WorkOut.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class WorkOut extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUlids;
 
-    protected $table = 'workOuts';
+    protected $table = 'workOut';
 
     protected $primaryKey = 'idWorkOut';
 
