688f0adc4d3866d977319edb9d58bb7764280970
andre <andrealbuquerque2001@gmail.com>
2024-06-13 20:25:01 -0300

    feat: update

--- app/src/app/fonts.ts
+++ app/src/app/fonts.ts
@@ -1,6 +1,9 @@
 import { Roboto } from 'next/font/google'
 
-export const roboto = Roboto({
-  weight: ['100', '300', '400', '500', '700', '900'],
-  style: ['normal', 'italic'],
+export const fontBody = Roboto({
+  weight: ['100', '300', '400', '500', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
 })
