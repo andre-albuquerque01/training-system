08d8df77602ba8a0c4dc57f31db6caf2714f6f74
andre <andrealbuquerque2001@gmail.com>
2024-06-13 20:24:44 -0300

    feat: create

---
+++ app/src/app/(dashboard)/user/insert/page.tsx
@@ -0,0 +1,17 @@
+import { InsertUserComponent } from '@/components/user/insert/InsertUserComponent'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s',
+    default: 'Insert user',
+  },
+}
+
+export default function InsertUser() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <InsertUserComponent />
+    </div>
+  )
+}
---
+++ app/src/app/(dashboard)/user/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginComponent } from '@/components/user/login/LoginComponent'
+
+export default function Login() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <LoginComponent />
+    </div>
+  )
+}
