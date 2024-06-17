e6364388ed167f5f75771e10693bcba20e26c8bf
andre <andrealbuquerque2001@gmail.com>
2024-06-17 17:52:11 -0300

    feat: create

---
+++ app/src/actions/trainingWorkOut/insert.ts
@@ -0,0 +1,33 @@
+'use server'
+
+import ApiAction from '@/functions/data/apiAction'
+import { RevalidateTag } from '@/functions/data/revalidateTag'
+import { cookies } from 'next/headers'
+
+export async function InsertTrainingWorkOut(request: object) {
+  try {
+    const response = await ApiAction('/trainingWorkOut', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + cookies().get('token')?.value,
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(request),
+    })
+
+    const data = await response.json()
+    console.log(data)
+
+    const message =
+      typeof data.message === 'string'
+        ? data.message
+        : JSON.stringify(data.message)
+
+    if (message && message.includes('The email has already been taken.'))
+      throw new Error('E-mail já cadastrado!')
+    RevalidateTag('training')
+  } catch (error) {
+    return 'Error'
+  }
+}
