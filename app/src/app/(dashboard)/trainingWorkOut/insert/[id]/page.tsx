b517b1c77af2929ee3e5cb40a9decd5e3dd3cfbb
andre <andrealbuquerque2001@gmail.com>
2024-06-17 17:52:14 -0300

    feat: create

---
+++ app/src/app/(dashboard)/trainingWorkOut/insert/[id]/page.tsx
@@ -0,0 +1,49 @@
+import ShowWorkOut, { WorkOutInterface } from '@/actions/workOut/showWorkOut'
+import { InsertTrainingWorkOutComponente } from '@/components/trainingWorkOut/insert'
+import Image from 'next/image'
+import Link from 'next/link'
+import { MdKeyboardArrowRight } from 'react-icons/md'
+
+export default async function InsertTrainingWorkout({
+  params,
+}: {
+  params: { id: string }
+}) {
+  const dt = (await ShowWorkOut()) as WorkOutInterface[]
+
+  return (
+    <div className="p-4">
+      <h1 className="font-bold py-2 text-lg">Listas de Exercícios</h1>
+      <div className="flex items-center flex-wrap gap-2">
+        {dt.length > 0 &&
+          dt &&
+          dt.map((data) => (
+            <div key={data.idWorkOut}>
+              <div className="border border-zinc-500 rounded-sm max-md:w-80 w-96  transform duration-500 hover:scale-105 hover:bg-zinc-800 hover:text-white">
+                <div className="relative flex items-center justify-between p-2">
+                  <Image
+                    src={`${data.image}`}
+                    width={100}
+                    height={100}
+                    alt="Imagem"
+                    className="w-[50%] max-h-20 min-h-10 object-contain"
+                  />
+                  <div className="w-[50%]">
+                    <p className="font-medium">{data.name}</p>
+                    <p className="font-light">{data.repetition}</p>
+                  </div>
+                  <Link href={`/workOut/${data.idWorkOut}`}>
+                    <MdKeyboardArrowRight className="w-5 h-8" />
+                  </Link>
+                  <InsertTrainingWorkOutComponente
+                    idWorkOut={data.idWorkOut}
+                    idTrainingType={params.id}
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  )
+}
