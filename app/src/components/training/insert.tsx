4538d2d419d40a8554b2181313ef97239d91c91e
andre <andrealbuquerque2001@gmail.com>
2024-06-17 17:52:21 -0300

    feat: create

---
+++ app/src/components/training/insert.tsx
@@ -0,0 +1,62 @@
+'use client'
+import Link from 'next/link'
+import { useFormState, useFormStatus } from 'react-dom'
+import { GoArrowLeft } from 'react-icons/go'
+import { InputComponent } from '@/components/form/inputComponent'
+import { InsertTrainingType } from '@/actions/trainingType/insert'
+
+function FormButton() {
+  const { pending } = useFormStatus()
+
+  return (
+    <>
+      {pending ? (
+        <button
+          className="bg-blue-500 text-white px-4 py-2 w-96 max-md:w-80 max-md:mx-auto rounded-lg"
+          disabled={pending}
+        >
+          Cadastrando...
+        </button>
+      ) : (
+        <button
+          className="bg-blue-500 text-white px-4 py-2 w-96 max-md:w-80 max-md:mx-auto rounded-lg"
+          disabled={pending}
+        >
+          Cadastrar
+        </button>
+      )}
+    </>
+  )
+}
+
+export const InsertTrainingComponent = () => {
+  const [state, action] = useFormState(InsertTrainingType, {
+    ok: false,
+    error: '',
+    data: null,
+  })
+
+  return (
+    <form className="space-y-2 flex flex-col text-black" action={action}>
+      <Link href="/workOut" className="flex items-center">
+        <GoArrowLeft className="w-5 h-5" /> Voltar
+      </Link>
+      <InputComponent
+        type="text"
+        label="Titúlo"
+        name="name"
+        id="name"
+        required={true}
+      />
+      <InputComponent
+        type="text"
+        label="Descrição"
+        name="description"
+        id="description"
+        required={true}
+      />
+      <p className="text-xs text-red-600">{state && state.error}</p>
+      <FormButton />
+    </form>
+  )
+}
