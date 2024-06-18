99739b8ba6061765256e0cd0415686f27a36412e
andre <andrealbuquerque2001@gmail.com>
2024-06-18 12:36:28 -0300

    feat: create

---
+++ app/src/actions/user/show.ts
@@ -0,0 +1,42 @@
+'use server'
+
+import ApiAction from '@/functions/data/apiAction'
+import apiError from '@/functions/error/apiErro'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { WorkOutInterface } from '../workOut/showWorkOut'
+
+export interface TrainingInterface {
+  idTrainingWorkOut: string
+  workOut: WorkOutInterface
+}
+
+export interface UserInterface {
+  name: string
+  email: string
+}
+
+export default async function User() {
+  try {
+    const response = await ApiAction('/user/show', {
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + cookies().get('token')?.value,
+      },
+      cache: 'no-cache',
+      // next: {
+      //   revalidate: 30 * 60,
+      //   tags: ['user'],
+      // },
+    })
+    const data = await response.json()
+    if (data.message === 'Unauthenticated.' || !data) {
+      cookies().delete('token')
+      redirect('/')
+    }
+
+    return data.data
+  } catch (error) {
+    return apiError(error)
+  }
+}
