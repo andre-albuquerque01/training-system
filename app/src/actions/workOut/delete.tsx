0548c4c1c470102eecc78cede888e1c057e87b32
andre <andrealbuquerque2001@gmail.com>
2024-06-18 12:36:43 -0300

    feat: update

--- app/src/actions/workOut/delete.tsx
+++ app/src/actions/workOut/delete.tsx
@@ -1,36 +1,24 @@
 'use server'
 
 import ApiAction from '@/functions/data/apiAction'
+import { RevalidateTag } from '@/functions/data/revalidateTag'
 import apiError from '@/functions/error/apiErro'
 import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
 
 export async function DeleteWorkOut(idWorkOut: string) {
   try {
     if (!idWorkOut) {
       throw new Error('Preenchas os dados!')
     }
-
-    const response = await ApiAction(`/work/${idWorkOut}`, {
+    await ApiAction(`/work/${idWorkOut}`, {
       method: 'DELETE',
       headers: {
         Accept: 'application/json',
         Authorization: 'Bearer ' + cookies().get('token')?.value,
       },
     })
-
-    const data = await response.json()
-    console.log(data)
-
-    const message =
-      typeof data.message === 'string'
-        ? data.message
-        : JSON.stringify(data.message)
-
-    if (message && message.includes('The email has already been taken.'))
-      throw new Error('E-mail já cadastrado!')
   } catch (error) {
     return apiError(error)
   }
-  redirect('/')
+  RevalidateTag('workOut')
 }
