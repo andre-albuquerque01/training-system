025ff689d329cbd124e89412fc5a935214e51ac3
andre <andrealbuquerque2001@gmail.com>
2024-06-18 12:36:48 -0300

    feat: update

--- app/src/actions/workOut/showWorkOut.tsx
+++ app/src/actions/workOut/showWorkOut.tsx
@@ -29,10 +29,10 @@ export default async function ShowWorkOut() {
         Accept: 'application/json',
         Authorization: 'Bearer ' + cookies().get('token')?.value,
       },
-      cache: 'no-cache',
-      // next: {
-      //   revalidate: 30 * 60,
-      // },
+      next: {
+        revalidate: 30 * 60,
+        tags: ['workOut'],
+      },
     })
     const data = await response.json()
 
