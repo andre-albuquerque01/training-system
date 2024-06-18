356eec454ffe1da886e834548e7b30cb069eba64
andre <andrealbuquerque2001@gmail.com>
2024-06-18 12:36:53 -0300

    feat: update

--- app/src/app/(dashboard)/dashboard/page.tsx
+++ app/src/app/(dashboard)/dashboard/page.tsx
@@ -1,14 +1,25 @@
 import { TrainingTypeInterface } from '@/actions/trainingType/showOneTraining'
 import ShowTrainingType from '@/actions/trainingType/showTraining'
 import Link from 'next/link'
+import { CiCirclePlus } from 'react-icons/ci'
 import { GoArrowRight } from 'react-icons/go'
 
 export default async function Dashboard() {
   const data = (await ShowTrainingType()) as TrainingTypeInterface[]
 
   return (
-    <div className="mx-auto min-h-screen flex justify-center">
-      <div className="p-4 space-y-4">
+    <div className="p-4">
+      <div className=" inline-block">
+        <Link
+          href="/training/insert"
+          className="flex items-center gap-1 hover:text-blue-500"
+        >
+          <CiCirclePlus className="w-5 h-5" />
+          Adicionar itens
+        </Link>
+      </div>
+      <h1 className="font-bold py-2 text-lg">Meu treino</h1>
+      <div className="flex w-full items-center flex-wrap gap-2">
         {data.map((itens) => (
           <Link
             href={`/training/${itens.id}`}
