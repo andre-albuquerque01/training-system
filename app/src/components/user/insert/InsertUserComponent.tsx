36ff588b6b1e4d13838b110ccad8c57fb1974af9
andre <andrealbuquerque2001@gmail.com>
2024-06-19 12:13:50 -0300

    feat: update

--- app/src/components/user/insert/InsertUserComponent.tsx
+++ app/src/components/user/insert/InsertUserComponent.tsx
@@ -12,14 +12,14 @@ function FormButton() {
     <>
       {pending ? (
         <button
-          className="bg-red-600 text-white px-4 py-2 w-96 max-md:w-80 max-md:mx-auto rounded-lg"
+          className="bg-blue-600 text-white px-4 py-2 w-96 max-md:w-80 max-md:mx-auto rounded-lg"
           disabled={pending}
         >
           Cadastrando...
         </button>
       ) : (
         <button
-          className="bg-red-600 text-white px-4 py-2 w-96 max-md:w-80 max-md:mx-auto rounded-lg"
+          className="bg-blue-600 text-white px-4 py-2 w-96 max-md:w-80 max-md:mx-auto rounded-lg"
           disabled={pending}
         >
           Cadastrar
