7526a50dca4994fa25215b73140c40d5ec188809
andre <andrealbuquerque2001@gmail.com>
2024-06-19 12:13:39 -0300

    feat: update

--- app/src/app/(dashboard)/workOut/page.tsx
+++ app/src/app/(dashboard)/workOut/page.tsx
@@ -25,15 +25,27 @@ export default async function WorkOut() {
           dt &&
           dt.map((data) => (
             <div key={data.idWorkOut}>
-              <div className="border border-zinc-500 rounded-sm max-md:w-80 w-96  transform duration-500 hover:scale-105 hover:bg-zinc-800 hover:text-white">
-                <div className="relative flex items-center justify-between p-2">
-                  <Image
-                    src={`${data.image}`}
-                    width={100}
-                    height={100}
-                    alt="Imagem"
-                    className="w-[50%] max-h-20 min-h-10 object-contain"
-                  />
+              <div className="border border-zinc-500 rounded-sm max-md:w-80 w-96 transform duration-500 hover:scale-105 hover:bg-zinc-800 hover:text-white">
+                <div className="relative flex items-center justify-between p-2 h-24">
+                  <div className="w-[50%]">
+                    {data.image && data.image.length > 10 ? (
+                      <Image
+                        src={`${data.image}`}
+                        width={100}
+                        height={100}
+                        alt="Imagem"
+                        className="max-w-28 max-h-20 min-h-10 object-contain mx-auto"
+                      />
+                    ) : (
+                      <Image
+                        src="/gym.jpg"
+                        width={100}
+                        height={100}
+                        alt="Imagem"
+                        className="max-w-28 max-h-20 min-h-10 object-contain mx-auto"
+                      />
+                    )}
+                  </div>
                   <div className="w-[50%]">
                     <p className="font-medium">{data.name}</p>
                     <p className="font-light">{data.repetition}</p>
